a1941f9d8264f90cd2bca2e6447f8c4d0d95fcf1
jmojzes21 <jmojzes21@student.foi.hr>
2025-10-28 17:20:58 +0100

    Napravi regex komande graditelj

---
+++ jmojzes21_zadaca_1/src/main/java/edu/unizg/foi/uzdiz/jmojzes21/zadaca_1/pomocnici/RegexKomandeGraditelj.java
@@ -0,0 +1,43 @@
+package edu.unizg.foi.uzdiz.jmojzes21.zadaca_1.pomocnici;
+
+import java.util.regex.Pattern;
+
+public class RegexKomandeGraditelj {
+
+  private StringBuilder regex;
+  private final String razmakArgumenata = "\\s+";
+
+  public RegexKomandeGraditelj(String nazivKomande) {
+    regex = new StringBuilder();
+    regex.append(String.format("(%s)", nazivKomande));
+  }
+
+  public RegexKomandeGraditelj dodajBroj(String naziv) {
+    regex.append(razmakArgumenata);
+    regex.append(String.format("(?<%s>\\d+)", naziv));
+    return this;
+  }
+
+  public RegexKomandeGraditelj dodajTekst(String naziv) {
+    regex.append(razmakArgumenata);
+    regex.append(String.format("(?<%s>\\w+)", naziv));
+    return this;
+  }
+
+  public RegexKomandeGraditelj dodajDatum(String naziv) {
+    regex.append(razmakArgumenata);
+    regex.append(String.format("(?<%s>\\d+\\.\\d+\\.\\d+\\.?)", naziv));
+    return this;
+  }
+
+  public RegexKomandeGraditelj dodajVrijeme(String naziv) {
+    regex.append(razmakArgumenata);
+    regex.append(String.format("(?<%s>\\d+\\:\\d+(\\:\\d+)?)", naziv));
+    return this;
+  }
+
+  public Pattern dajUzorak() {
+    return Pattern.compile(regex.toString());
+  }
+
+}
